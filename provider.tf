906b2d3aaf5fb5ea397b4d7d36e224bba7c7219f
Gaurav Rai <118634296+gaurav3366@users.noreply.github.com>
2023-05-27 04:24:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAR4EADLW2D2TJQENP"
-  secret_key = "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"
+  access_key = "AKIAR4EADLW2D2TJQE"
+  secret_key = "rbu+2E7zOj07iUHj6vvHkfmr1Wr+WajnJl/6TT"
 }
